577708774c1a961047e36dd320805da3a2ebdd2f
Jireh <twajxjiraffe@gmail.com>
2022-03-19 17:14:50 +0800

    Add missing from fork

--- main.tf
+++ main.tf
@@ -170,3 +170,28 @@ users:
     KUBECONFIG
   filename = "kubeconfig"
 }
+/*
+#  Use data to ensure that the cluster is up before we start using it
+data "aws_eks_cluster" "msur" {
+  name = aws_eks_cluster.ms-up-running.id
+}
+
+# Use kubernetes provider to work with the kubernetes cluster API
+provider "kubernetes" {
+  #load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.msur.endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+  }
+}
+
+# Create a namespace for microservice pods 
+resource "kubernetes_namespace" "ms-namespace" {
+  metadata {
+    name = var.ms_namespace
+  }
+}
+*/
